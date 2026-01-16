bccf6d57de8deb764e757789a1278615606d745d
JovaneshPiramoon <jovaneshpiramoon@gmail.com>
2026-01-16 14:26:31 +0500

    Add tailwind config ready to start !

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App/App"
-
+import "./mainStyle.css"
 const root =document.getElementById("root")
 
 if(!root) throw new Error("need selected root element to creartRoot of files !")
